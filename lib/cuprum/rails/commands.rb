8c05611a6837b5cea8089ae607770a2f268b0e97
Rob Smith <merlin@sleepingkingstudios.com>
2021-04-30 11:32:22 -0400

    Implement Commands::DestroyOne.

--- lib/cuprum/rails/commands.rb
+++ lib/cuprum/rails/commands.rb
@@ -5,7 +5,8 @@ require 'cuprum/rails'
 module Cuprum::Rails
   # Namespace for commands implementing Rails collection functionality.
   module Commands
-    autoload :AssignOne, 'cuprum/rails/commands/assign_one'
-    autoload :BuildOne,  'cuprum/rails/commands/build_one'
+    autoload :AssignOne,  'cuprum/rails/commands/assign_one'
+    autoload :BuildOne,   'cuprum/rails/commands/build_one'
+    autoload :DestroyOne, 'cuprum/rails/commands/destroy_one'
   end
 end
