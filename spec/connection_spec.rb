8f97af7a6a27ef700f8915af7c662f803cc17c06
Rob Smith <merlin@sleepingkingstudios.com>
2021-04-30 01:39:20 -0400

    Implement Book example model.

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'support/book'
+
 RSpec.describe 'SQL connection' do # rubocop:disable RSpec/DescribeClass
-  it { expect { ActiveRecord::Base.connection }.not_to raise_error }
+  it { expect { Book.connection }.not_to raise_error }
 end
