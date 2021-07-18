5e00408a84a81c3d2ddf27f56f87613bc742aaff
Rob Smith <merlin@sleepingkingstudios.com>
2021-07-18 17:42:39 -0400

    Implement Cuprum::Rails::Routing::SingularRoutes.

--- lib/cuprum/rails/routing.rb
+++ lib/cuprum/rails/routing.rb
@@ -5,6 +5,7 @@ require 'cuprum/rails'
 module Cuprum::Rails
   # Namespace for routing-specific functionality.
   module Routing
-    autoload :PluralRoutes, 'cuprum/rails/routing/plural_routes'
+    autoload :PluralRoutes,   'cuprum/rails/routing/plural_routes'
+    autoload :SingularRoutes, 'cuprum/rails/routing/singular_routes'
   end
 end
