ac3c91e9225afab7f4f120898fa9355c0617dcbf
Rob Smith <merlin@sleepingkingstudios.com>
2021-10-20 02:16:55 -0400

    Implement Serializers::Json.default_serializers.

--- lib/cuprum/rails/serializers/json.rb
+++ lib/cuprum/rails/serializers/json.rb
@@ -12,5 +12,22 @@ module Cuprum::Rails::Serializers
     autoload :IdentitySerializer,
       'cuprum/rails/serializers/json/identity_serializer'
     autoload :Serializer, 'cuprum/rails/serializers/json/serializer'
+
+    # Default serializers for handling value objects and data structures.
+    #
+    # @return [Hash<Class, Cuprum::Rails::Serializers::Json::Serializer>] the
+    #   default serializers.
+    def self.default_serializers
+      @default_serializers ||= {
+        Array      => self::ArraySerializer.instance,
+        Hash       => self::HashSerializer.instance,
+        FalseClass => self::IdentitySerializer.instance,
+        Float      => self::IdentitySerializer.instance,
+        Integer    => self::IdentitySerializer.instance,
+        NilClass   => self::IdentitySerializer.instance,
+        String     => self::IdentitySerializer.instance,
+        TrueClass  => self::IdentitySerializer.instance
+      }
+    end
   end
 end
---
+++ spec/cuprum/rails/serializers/json_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'cuprum/rails/serializers/json'
+
+RSpec.describe Cuprum::Rails::Serializers::Json do
+  describe '.default_serializers' do
+    let(:default_serializers) { described_class.default_serializers }
+    let(:expected_keys) do
+      [
+        Array,
+        FalseClass,
+        Float,
+        Hash,
+        Integer,
+        NilClass,
+        String,
+        TrueClass
+      ]
+    end
+
+    include_examples 'should define class reader',
+      :default_serializers,
+      -> { an_instance_of(Hash) }
+
+    it 'should define the default serializers' do
+      expect(default_serializers.keys).to contain_exactly(*expected_keys)
+    end
+
+    it 'should define the Array serializer' do
+      expect(default_serializers[Array])
+        .to be_a Cuprum::Rails::Serializers::Json::ArraySerializer
+    end
+
+    it 'should define the FalseClass serializer' do
+      expect(default_serializers[FalseClass])
+        .to be_a Cuprum::Rails::Serializers::Json::IdentitySerializer
+    end
+
+    it 'should define the Float serializer' do
+      expect(default_serializers[Float])
+        .to be_a Cuprum::Rails::Serializers::Json::IdentitySerializer
+    end
+
+    it 'should define the Hash serializer' do
+      expect(default_serializers[Hash])
+        .to be_a Cuprum::Rails::Serializers::Json::HashSerializer
+    end
+
+    it 'should define the Integer serializer' do
+      expect(default_serializers[Integer])
+        .to be_a Cuprum::Rails::Serializers::Json::IdentitySerializer
+    end
+
+    it 'should define the NilClass serializer' do
+      expect(default_serializers[NilClass])
+        .to be_a Cuprum::Rails::Serializers::Json::IdentitySerializer
+    end
+
+    it 'should define the String serializer' do
+      expect(default_serializers[String])
+        .to be_a Cuprum::Rails::Serializers::Json::IdentitySerializer
+    end
+
+    it 'should define the TrueClass serializer' do
+      expect(default_serializers[TrueClass])
+        .to be_a Cuprum::Rails::Serializers::Json::IdentitySerializer
+    end
+  end
+end
