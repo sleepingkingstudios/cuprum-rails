af493a245d0b3d4832cf76ce7abd0b8a64ca03c5
Rob Smith <merlin@sleepingkingstudios.com>
2022-01-06 08:13:33 -0500

    Implement configurable steps for Actions::ResourceAction.

--- lib/cuprum/rails/actions/resource_action.rb
+++ lib/cuprum/rails/actions/resource_action.rb
@@ -1,11 +1,68 @@
 # frozen_string_literal: true
 
+require 'cuprum/errors/uncaught_exception'
+
 require 'cuprum/rails/action'
 require 'cuprum/rails/actions/resource_methods'
 
 module Cuprum::Rails::Actions
   # Abstract base class for resourceful actions.
+  #
+  # Each ResourceAction defines a series of steps used to validate and process
+  # the action. Each step is performed in order:
+  #
+  # - The #validate_parameters step checks whether the request params are valid.
+  #   For example, it may assert that the primary key is given for a member
+  #   action, or that the attributes for a create or update action are present.
+  # - The #find_required_entities step locates any dependent entities and
+  #   ensures that the entities exist. For example, it may find the requested
+  #   entity for a show action, or the parent entity for a create action on a
+  #   nested resource.
+  # - The #perform_action step contains the core logic of the action. For
+  #   example, in a destroy action it would take the entity (located in the
+  #   previous step) and remove it from the collection.
+  # - The #build_response step generates the result returned by a successful
+  #   request. For example, for a show action on a nested resource, it might
+  #   build a passing result with both the requested and parent resources.
+  #
+  # If any of the steps fail, either by returning a failing result or by raising
+  # an exception, the action will immediately stop execution and return the
+  # result, or wrap the exception in a failing result with a
+  # Cuprum::Errors::UncaughtException error.
   class ResourceAction < Cuprum::Rails::Action
     include Cuprum::Rails::Actions::ResourceMethods
+
+    private
+
+    def build_response
+      Cuprum::Result.new(status: :success)
+    end
+
+    def find_required_entities; end
+
+    def handle_exceptions
+      yield
+    rescue StandardError => exception
+      error = Cuprum::Errors::UncaughtException.new(
+        exception: exception,
+        message:   "uncaught exception in #{self.class.name} -"
+      )
+      failure(error)
+    end
+
+    def perform_action; end
+
+    def process(request:)
+      super
+
+      handle_exceptions do
+        step { validate_parameters }
+        step { find_required_entities }
+        step { perform_action }
+        step { build_response }
+      end
+    end
+
+    def validate_parameters; end
   end
 end
--- spec/cuprum/rails/actions/resource_action_spec.rb
+++ spec/cuprum/rails/actions/resource_action_spec.rb
@@ -21,4 +21,181 @@ RSpec.describe Cuprum::Rails::Actions::ResourceAction do
   let(:resource_options) { {} }
 
   include_examples 'should define the ResourceAction methods'
+
+  describe '#call' do
+    shared_examples 'should call the previous action steps' do |method_name|
+      let(:included_steps) do
+        [*action_steps.split(method_name).first, method_name]
+      end
+      let(:excluded_steps) do
+        action_steps.split(method_name).last
+      end
+
+      it 'should call the previous action steps', :aggregate_failures do # rubocop:disable RSpec/ExampleLength
+        mocked_action.call(request: request)
+
+        included_steps.each do |step|
+          expect(mocked_action).to have_received(step)
+        end
+
+        excluded_steps.each do |step|
+          expect(mocked_action).not_to have_received(step)
+        end
+      end
+    end
+
+    shared_examples 'should call the action step' do |method_name|
+      context "when the ##{method_name} step raises an exception" do
+        let(:exception) { StandardError.new('Something went wrong') }
+        let(:expected_message) do
+          'uncaught exception in Cuprum::Rails::Actions::ResourceAction -'
+        end
+        let(:expected_error) do
+          Cuprum::Errors::UncaughtException.new(
+            exception: exception,
+            message:   expected_message
+          )
+        end
+
+        before(:example) do
+          allow(mocked_action).to receive(method_name).and_raise(exception)
+        end
+
+        it 'should return the failing result' do
+          expect(mocked_action.call(request: request))
+            .to be_a_failing_result
+            .with_error(expected_error)
+        end
+
+        include_examples 'should call the previous action steps', method_name
+      end
+
+      context "when the ##{method_name} step returns a failing result" do
+        let(:expected_error) do
+          Cuprum::Error.new(message: 'Something went wrong')
+        end
+        let(:result) do
+          Cuprum::Result.new(error: expected_error)
+        end
+
+        before(:example) do
+          allow(mocked_action).to receive(method_name).and_return(result)
+        end
+
+        it 'should return the failing result' do
+          expect(mocked_action.call(request: request))
+            .to be_a_failing_result
+            .with_error(expected_error)
+        end
+
+        include_examples 'should call the previous action steps', method_name
+      end
+
+      context 'when the action defines custom exception handling' do
+        let(:described_class) { Spec::Action }
+
+        # rubocop:disable RSpec/DescribedClass
+        example_class 'Spec::Action', Cuprum::Rails::Actions::ResourceAction \
+        do |klass|
+          klass.define_method(:handle_exceptions) do |&block|
+            super() do
+              block.call
+            rescue Spec::CustomException
+              error = Cuprum::Error.new(message: 'Something went wrong')
+
+              failure(error)
+            end
+          end
+        end
+        # rubocop:enable RSpec/DescribedClass
+
+        example_class 'Spec::CustomException', StandardError
+
+        context "when the ##{method_name} step raises a custom exception" do
+          let(:exception) { Spec::CustomException.new('Something went wrong') }
+          let(:expected_error) do
+            Cuprum::Error.new(message: 'Something went wrong')
+          end
+
+          before(:example) do
+            allow(mocked_action).to receive(method_name).and_raise(exception)
+          end
+
+          it 'should return the failing result' do
+            expect(mocked_action.call(request: request))
+              .to be_a_failing_result
+              .with_error(expected_error)
+          end
+
+          include_examples 'should call the previous action steps', method_name
+        end
+
+        context "when the ##{method_name} step raises a standard exception" do
+          let(:exception) { StandardError.new('Something went wrong') }
+          let(:expected_message) do
+            'uncaught exception in Spec::Action -'
+          end
+          let(:expected_error) do
+            Cuprum::Errors::UncaughtException.new(
+              exception: exception,
+              message:   expected_message
+            )
+          end
+
+          before(:example) do
+            allow(mocked_action).to receive(method_name).and_raise(exception)
+          end
+
+          it 'should return the failing result' do
+            expect(mocked_action.call(request: request))
+              .to be_a_failing_result
+              .with_error(expected_error)
+          end
+
+          include_examples 'should call the previous action steps', method_name
+        end
+      end
+    end
+
+    let(:params)  { {} }
+    let(:request) { instance_double(ActionDispatch::Request, params: params) }
+    let(:action_steps) do
+      %i[
+        validate_parameters
+        find_required_entities
+        perform_action
+        build_response
+      ]
+    end
+    let(:mocked_action) do
+      action.tap do |mock|
+        action_steps.each do |method_name|
+          allow(mock).to receive(method_name)
+        end
+      end
+    end
+
+    it 'should return a passing result' do
+      expect(mocked_action.call(request: request))
+        .to be_a_passing_result
+        .with_value(nil)
+    end
+
+    it 'should call each action step', :aggregate_failures do
+      mocked_action.call(request: request)
+
+      expect(mocked_action).to have_received(:validate_parameters)
+      expect(mocked_action).to have_received(:find_required_entities)
+      expect(mocked_action).to have_received(:perform_action)
+      expect(mocked_action).to have_received(:build_response)
+    end
+
+    include_examples 'should call the action step', :build_response
+
+    include_examples 'should call the action step', :find_required_entities
+
+    include_examples 'should call the action step', :perform_action
+
+    include_examples 'should call the action step', :validate_parameters
+  end
 end
