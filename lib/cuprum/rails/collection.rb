06a3f16c5d9edf6253e85db727c1061e236961fa
Rob Smith <sleepingkingstudios@gmail.com>
2024-09-03 15:24:15 -0400

    Autodetect Collection primary key name and type.

--- lib/cuprum/rails/collection.rb
+++ lib/cuprum/rails/collection.rb
@@ -44,6 +44,21 @@ module Cuprum::Rails
       Cuprum::Rails::Commands::ValidateOne.new(**command_options)
     end
 
+    # @return [String] the name of the primary key attribute.
+    def primary_key_name
+      @primary_key_name ||=
+        options.fetch(:primary_key_name, entity_class&.primary_key || 'id').to_s
+    end
+
+    # @return [Class, Stannum::Constraint] the type of the primary key
+    #   attribute. Defaults to Integer.
+    def primary_key_type
+      @primary_key_type ||=
+        options
+          .fetch(:primary_key_type, resolve_primary_key_type)
+          .then { |obj| obj.is_a?(String) ? Object.const_get(obj) : obj }
+    end
+
     # A new Query instance, used for querying against the collection data.
     #
     # @return [Cuprum::Rails::Query] the query.
@@ -61,6 +76,26 @@ module Cuprum::Rails
 
     def default_scope
       Cuprum::Rails::Scopes::AllScope.new
+    end
+
+    def primary_key_column_type
+      entity_class
+        &.columns
+        &.find { |column| column.name == primary_key_name }
+        &.type
+    end
+
+    def resolve_primary_key_type
+      column_type = primary_key_column_type
+
+      return Integer unless column_type
+
+      case column_type
+      when :string, :uuid
+        String
+      else
+        Integer
+      end
     end
   end
 end
--- spec/cuprum/rails/collection_spec.rb
+++ spec/cuprum/rails/collection_spec.rb
@@ -33,4 +33,40 @@ RSpec.describe Cuprum::Rails::Collection do
 
   include_contract 'should be a collection',
     commands_namespace: 'Cuprum::Rails::Commands'
+
+  describe '#primary_key_name' do
+    context 'when the record class defines a custom primary key' do
+      let(:name)         { 'tomes' }
+      let(:entity_class) { Tome }
+
+      it { expect(collection.primary_key_name).to be == 'uuid' }
+
+      context 'when initialized with primary_key_name: a String' do
+        let(:primary_key_name) { 'id' }
+        let(:constructor_options) do
+          super().merge(primary_key_name:)
+        end
+
+        it { expect(collection.primary_key_name).to be == primary_key_name }
+      end
+    end
+  end
+
+  describe '#primary_key_type' do
+    context 'when the record class defines a custom primary key' do
+      let(:name)         { 'tomes' }
+      let(:entity_class) { Tome }
+
+      it { expect(collection.primary_key_type).to be String }
+
+      context 'when initialized with primary_key_name: a Class' do
+        let(:primary_key_type) { Integer }
+        let(:constructor_options) do
+          super().merge(primary_key_type:)
+        end
+
+        it { expect(collection.primary_key_type).to be primary_key_type }
+      end
+    end
+  end
 end
